8a1ce392e6fd830d2b5f5cacf1006deebf6c153d
golden <amjadhamdoun0@gmail.com>
2022-04-30 01:09:04 +0300

    widget

---
+++ lib/feature/Pages/HomePage/model/allFriend/all_friend_data_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'all_friend_data_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AllFriendDataModel _$AllFriendDataModelFromJson(Map<String, dynamic> json) =>
+    AllFriendDataModel(
+      img: json['img'] as String?,
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$AllFriendDataModelToJson(AllFriendDataModel instance) =>
+    <String, dynamic>{
+      'img': instance.img,
+      'id': instance.id,
+      'name': instance.name,
+    };
