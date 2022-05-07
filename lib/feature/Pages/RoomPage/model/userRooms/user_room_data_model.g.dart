ae4bdc606be5c058b38077c9147b1a99b71ef8b5
golden <amjadhamdoun0@gmail.com>
2022-05-07 14:31:03 +0400

    widget

---
+++ lib/feature/Pages/RoomPage/model/userRooms/user_room_data_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_room_data_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserRoomDataModel _$UserRoomDataModelFromJson(Map<String, dynamic> json) =>
+    UserRoomDataModel(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$UserRoomDataModelToJson(UserRoomDataModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
